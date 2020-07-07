6f04e8471bdc73d5a6fde3429dc1bee7ce8700ee
Matheus <dev@perugluglu>
2020-07-07 11:48:33 -0300

    Exceptions

---
+++ src/TesteConexao.java
@@ -0,0 +1,12 @@
+
+public class TesteConexao {
+	
+	public static void main(String[] args) {
+		
+		try (Conexao conexao = new Conexao()){
+			conexao.leDados();
+		} catch(IllegalStateException ex) {
+			System.out.println("Deu Erro Na Conexão");
+		} 
+	}
+}
